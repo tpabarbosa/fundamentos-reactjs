0b0bb95f77e0dfda9a7bd2bb325f747b48641991
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2020-11-11 22:21:58 -0300

    Fundamentos de ReactJs

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -30,6 +30,10 @@ export const Container = styled.div<ContainerProps>`
         &:hover {
           opacity: 0.6;
         }
+
+        &.active {
+          border-bottom: 2px solid #ff872c;
+        }
       }
     }
   }
